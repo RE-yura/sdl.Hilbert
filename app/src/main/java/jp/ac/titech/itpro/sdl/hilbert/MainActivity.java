4c8fa79cb36031161726669634d6765a1218a9f6
yura <yuraa.10345.tenn1s@gmail.com>
2020-05-15 21:24:57 +0900

    画面の回転に対応

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -3,64 +3,79 @@ package jp.ac.titech.itpro.sdl.hilbert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static int MAX_ORDER = 9;
-    private int order = 1;
+  private final static String TAG = "MainActivity";
+  private final static String KEY_NAME = "MainActivity.name";
 
-    private TextView orderView;
-    private HilbertView hilbertView;
-    private Button decButton;
-    private Button incButton;
+  private final static int MAX_ORDER = 9;
+  private int order = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private TextView orderView;
+  private HilbertView hilbertView;
+  private Button decButton;
+  private Button incButton;
 
-        orderView = findViewById(R.id.order_view);
-        hilbertView = findViewById(R.id.hilbert_view);
-        decButton = findViewById(R.id.dec_button);
-        incButton = findViewById(R.id.inc_button);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        decButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                assertTrue(order > 1, "A room to decrement order should exist");
-                order--;
-                display();
-            }
-        });
-        incButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                assertTrue(order < MAX_ORDER, "A room to increment order should exist");
-                order++;
-                display();
-            }
-        });
+    if (savedInstanceState != null) {
+      order = savedInstanceState.getInt(KEY_NAME);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    orderView = findViewById(R.id.order_view);
+    hilbertView = findViewById(R.id.hilbert_view);
+    decButton = findViewById(R.id.dec_button);
+    incButton = findViewById(R.id.inc_button);
+
+    decButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        assertTrue(order > 1, "A room to decrement order should exist");
+        order--;
         display();
-    }
+      }
+    });
+    incButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        assertTrue(order < MAX_ORDER, "A room to increment order should exist");
+        order++;
+        display();
+      }
+    });
+  }
 
-    private void display() {
-        orderView.setText(getString(R.string.order_view_format, order));
-        hilbertView.setOrder(order);
-        decButton.setEnabled(order > 1);
-        incButton.setEnabled(order < MAX_ORDER);
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    display();
+  }
 
-    public static void assertTrue(boolean f, String message) {
-        if (BuildConfig.DEBUG && !f) {
-            throw new AssertionError(message);
-        }
+  private void display() {
+    orderView.setText(getString(R.string.order_view_format, order));
+    hilbertView.setOrder(order);
+    decButton.setEnabled(order > 1);
+    incButton.setEnabled(order < MAX_ORDER);
+  }
+
+  public static void assertTrue(boolean f, String message) {
+    if (BuildConfig.DEBUG && !f) {
+      throw new AssertionError(message);
+    }
+  }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_NAME, order);
     }
 }
